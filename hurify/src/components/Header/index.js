7f6e714c3c759c9d289c4bf88e5f8ea9a5bca8ad
Huriel Lopes <huriel@exemplo.com>
2020-12-17 11:02:17 -0300

    hurify

---
+++ hurify/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return(
+    <Container>
+      <h1>Hurify</h1>
+    </Container>
+  );
+}
