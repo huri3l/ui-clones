29c86da95904787c830699e1d3b3d8fd0df92723
Huriel Lopes <huriel-lopes@hotmail.com>
2020-12-25 07:17:00 -0300

    finalização das páginas de profile e repo

--- github/src/pages/Repo/index.tsx
+++ github/src/pages/Repo/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, LinkButton, ForkIcon, GithubIcon } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/huri3l'}>
+          huri3l
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/huri3l/til'}>
+          til
+        </Link>
+      </Breadcrumb>
+
+      <p>Today I Learned</p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/huri3l/til'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
